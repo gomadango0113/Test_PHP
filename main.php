5f829d875f5358b6c0b52e53093b3cfb8dab5c1c
gomadango0113 <gomadango0113@gmail.com>
2025-10-26 22:15:28 +0900

    【追加】データベースに接続し、select文を実行できるように

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
 <?php
+$usr = ''; // 接続ユーザー名
+$pw = ''; // 接続時のパスワード
+$dsn = 'mysql:host=; dbname=testdb; charset=utf8';
+
+$pdo = new PDO($dsn, $usr, $pw);
+$spl = $pdo -> prepare('select age from test where id = 2');
+$spl->execute();
+$row = $spl -> fetch(PDO::FETCH_ASSOC);
+print_r($row['age']);
+
 print 'Hello World';
 ?>
